76720ab7bb59a2247ddd6232d8c4acbb0a9dd691
Denny Liang Faan <liang.faan@gmail.com>
2020-03-14 11:39:26 +0800

    update gatsby website

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,17 +6,26 @@
 
 module.exports = {
   /* Your site config here */
+  pathPrefix: "/gatsby-website",
+  siteMetadata: {
+    title: "Gatsby React Bootstrap Website",
+    description: "A starter that includes react-bootstrap and react-icons, along with SASS compilation.",
+    author: "Liang Faan",
+    email: "liang.faan@gmail.com"
+  },
   plugins: [
     {
-      resolve: `gatsby-plugin-typography`,
+      resolve: "gatsby-plugin-typography",
       options: {
-        pathToConfigModule: `src/utils/typography`,
+        pathToConfigModule: "src/utils/typography",
       },
     },
     {
-      resolve: `gatsby-plugin-s3`,
+      resolve: "gatsby-plugin-s3",
       options: {
         bucketName: "test-gatsby-website-bucket",
+        protocol: "https",
+        hostname: "test-gatsby-website-bucket.s3-website-us-east-1.amazonaws.com",
       },
     },
   ],
